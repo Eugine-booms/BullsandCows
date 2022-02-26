4a28c39406dc8f3c46f009aaf1ebb7acbe84f40c
Евгений Нестеров <e.y.nesterov@gmail.com>
2022-02-26 12:08:05 +0300

    Класс Programm

---
+++ WpfApp1/Programm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using WpfApp1;
+
+namespace BullsAndCowsWPF
+{
+    public static class Programm
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
