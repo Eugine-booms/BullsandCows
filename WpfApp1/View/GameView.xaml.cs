316dbb9d99bd96d1ff161b78acca4979eeb8cfc2
Евгений Нестеров <e.y.nesterov@gmail.com>
2026-06-27 21:14:11 +0300

    feat: Enter to submit guess + auto-focus after check
    - GameView: add KeyBinding Enter -> MakeGuessCommand
    - GameView: add x:Name=GuessTextBox
    - GameView code-behind: subscribe to UserGuess PropertyChanged
    - Auto-focus TextBox when UserGuess becomes empty after guess

--- WpfApp1/View/GameView.xaml.cs
+++ WpfApp1/View/GameView.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Windows.Controls;
 
 namespace BullsAndCowsWPF.View
@@ -7,6 +8,30 @@ namespace BullsAndCowsWPF.View
         public GameView()
         {
             InitializeComponent();
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        private void OnDataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue is INotifyPropertyChanged oldVm)
+                oldVm.PropertyChanged -= OnViewModelPropertyChanged;
+            if (e.NewValue is INotifyPropertyChanged newVm)
+                newVm.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ViewModels.GameViewModel.UserGuess))
+            {
+                var vm = DataContext as ViewModels.GameViewModel;
+                if (vm != null && string.IsNullOrEmpty(vm.UserGuess))
+                {
+                    Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Input, new System.Action(() =>
+                    {
+                        GuessTextBox.Focus();
+                    }));
+                }
+            }
         }
     }
 }
